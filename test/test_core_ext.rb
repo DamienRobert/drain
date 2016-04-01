2ffe658915ac5e6262c4d87ecaf3a0edab1fe50e
Damien Robert <damien.olivier.robert+git@gmail.com>
2016-04-01 15:43:54 +0200

    Test Hash#deep_merge

--- test/test_core_ext.rb
+++ test/test_core_ext.rb
@@ -14,6 +14,14 @@ describe DR::CoreExt do
 		h1.deep_merge(h2) { |key, old, new| Array(old) + Array(new) }.must_equal({:x=>{:y=>[4, 5, 6, 7, 8, 9]}, :z=>[7, 8, 9, "xyz"]})
 	end
 
+	it "Hash#deep_merge merge array when they start with nil" do
+		h1 = { x: { y: [4,5,6] }, z: [7,8,9] }
+		h2 = { x: { y: [nil, 7,8,9] }, z: 'xyz' }
+		h1.deep_merge(h2).must_equal({x: {y: [4,5,6,7, 8, 9]}, z: "xyz"})
+		{x: { y: []} }.deep_merge(h2).must_equal({x: {y: [7, 8, 9]}, z: "xyz"})
+		{z: "foo"}.deep_merge(h2).must_equal({x: {y: [7, 8, 9]}, z: "xyz"})
+	end
+
 	it "Implements Hash#inverse" do
 		h={ploum: 2, plim: 2, plam: 3}
 		h.inverse.must_equal({2=>[:ploum, :plim], 3=>[:plam]})
