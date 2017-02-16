681d9019f2380d8055e84422de1e047d962f60c7
Damien Robert <damien.olivier.robert+git@gmail.com>
2017-02-16 21:35:42 +0100

    Try to do a minimal example for ruby's segfault
    ruby 2.4.0p0 segfaults (but not always :-() when running test. Try to
    identify the cause, is it a cyclic structure?

--- test/helper.rb
+++ test/helper.rb
@@ -3,3 +3,4 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
 #Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+#Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
