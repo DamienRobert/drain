a65dfe0d007f06c234478924078384bea5526d40
Damien Robert <damien.olivier.robert+git@gmail.com>
2018-07-16 22:58:56 +0200

    bool.rb: allow to keep Strings/Symbols

--- lib/dr/base/bool.rb
+++ lib/dr/base/bool.rb
@@ -1,12 +1,13 @@
 module DR
   module Bool
     extend(self)
-    def to_bool(el, default=nil, allow_nil: true)
+    def to_bool(el, default=nil, allow_nil: true, string_fallback: true)
       case el
       when String
         string=el.chomp
         return true if string =~ (/(true|t|yes|y|1)$/i)
         return false if string.empty? || string =~ (/(false|f|no|n|0)$/i)
+        return el if string_fallback
       when Integer
         return ! (el == 0)
       when ::Process::Status
@@ -18,6 +19,7 @@ module DR
         #we don't return !!el because we don't want nil to be false but to
         #give an error
       end
+      return el if string_fallback and el.is_a?(Symbol)
       return default unless default.nil?
       return nil if el.nil? and allow_nil
       raise ArgumentError.new("Invalid value for Boolean: \"#{el}\"")
