2d17bad6a4c5f8ae3106bee589b80721d6a92d84
Damien Robert <damien.olivier.robert+git@gmail.com>
2018-08-03 14:41:40 +0200

    simple_keywords: switch from a Module to a Class

--- lib/dr/parse/simple_keywords.rb
+++ lib/dr/parse/simple_keywords.rb
@@ -1,26 +1,30 @@
 module DR
-	module SimpleKeywordsParser
-		extend self
+	class SimpleKeywordsParser
+		attr_accessor :opts, :keywords
 
-		#shortcut when we only have one keyword
-		def keyword(msg, kw, **opts, &b)
-			h={kw => b}
-			return keywords(msg,h,**opts)
+		def initialize(hash, **opts)
+			@opts=opts
+			@keywords=hash
 		end
 
-		def keywords(msg, hash, **opts)
+		def keyword(name, &b)
+			@keywords[name]=b
+		end
+
+		def parse(msg, **opts)
+			opts=@opts.merge(opts)
 			sep=opts[:sep] || /,\s*/
-			keywords=hash.keys
+			keywords=@keywords.keys
 			keywords_r="(?:"+keywords.map {|k| "(?:"+k+")"}.join("|")+")"
 			reg = %r{(?<kw>#{keywords_r})(?<re>\((?:(?>[^()]+)|\g<re>)*\))}
 			if (m=reg.match(msg))
 				arg=m[:re][1...m[:re].length-1]
-				arg=keywords(arg,hash,**opts)
+				arg=parse(arg, **opts)
 				args=arg.split(sep)
 				key=keywords.find {|k| /#{k}/ =~ m[:kw]}
-				r=hash[key].call(*args).to_s
-				msg=m.pre_match+r+keywords(m.post_match,hash,**opts)
-				msg=keywords(msg,hash,**opts) if opts[:recursive]
+				r=@keywords[key].call(*args).to_s
+				msg=m.pre_match+r+parse(m.post_match,**opts)
+				msg=keywords(msg,@keywords,**opts) if opts[:recursive]
 			end
 			return msg
 		end
--- test/test_simple_keywords.rb
+++ test/test_simple_keywords.rb
@@ -3,13 +3,22 @@ require 'dr/parse/simple_keywords'
 
 describe DR::SimpleKeywordsParser do
 	before do
-		@hash={
+		@parser=DR::SimpleKeywordsParser.new({
 			'FOO' => lambda { |*args|  "FOO: #{args}" },
 			'BAR' => lambda { |*args|  "BAR: #{args}" },
-		}
+		})
 	end
 
 	it "Can parse keywords" do
-		DR::SimpleKeywordsParser.keywords("FOO(ploum, plam)", @hash).must_equal 'FOO: ["ploum", "plam"]'
+		@parser.parse("FOO(ploum, plam)").must_equal 'FOO: ["ploum", "plam"]'
+	end
+
+	it "Can parse keywords inside keywords" do
+		@parser.parse("FOO(ploum, BAR( foo, bar )").must_equal 'FOO: ["ploum", "plam"]'
+	end
+
+	it "Can add a keyword" do
+		@parser.keyword("PLOUM") { |a,b| a.to_i+b.to_i}
+		@parser.parse("Hello PLOUM(2,3)").must_equal 'Hello 5'
 	end
 end
